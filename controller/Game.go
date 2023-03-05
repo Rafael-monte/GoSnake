98023c1fcf082a91d8a742087cb563c2c5865914
Rafael-monteiro <rafaelmonte46@gmail.com>
2023-03-05 11:03:20 -0300

    Input listener's code cleaned

--- controller/Game.go
+++ controller/Game.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"GoSnake/model"
 	"GoSnake/view"
-	"os"
 	"time"
 
 	"github.com/gdamore/tcell"
@@ -35,17 +34,6 @@ func (game *Game) ListenKeyEvents() {
 	case *tcell.EventResize:
 		game.Screen.Sync()
 	case *tcell.EventKey:
-		if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
-			game.Screen.Fini()
-			os.Exit(0)
-		} else if event.Key() == tcell.KeyUp {
-			game.Snake.ChangeDirection(view.Coordinate{Y: -1, X: 0})
-		} else if event.Key() == tcell.KeyDown {
-			game.Snake.ChangeDirection(view.Coordinate{Y: 1, X: 0})
-		} else if event.Key() == tcell.KeyLeft {
-			game.Snake.ChangeDirection(view.Coordinate{Y: 0, X: -1})
-		} else if event.Key() == tcell.KeyRight {
-			game.Snake.ChangeDirection(view.Coordinate{Y: 0, X: 1})
-		}
+		inputController[event.Key()](game)
 	}
 }
--- view/Measurements.go
+++ view/Measurements.go
@@ -4,6 +4,13 @@ type Coordinate struct {
 	X, Y int
 }
 
+var (
+	Up    Coordinate = Coordinate{Y: -1, X: 0}
+	Down  Coordinate = Coordinate{Y: 1, X: 0}
+	Left  Coordinate = Coordinate{Y: 0, X: -1}
+	Right Coordinate = Coordinate{Y: 0, X: 1}
+)
+
 type ScreenMeasurements struct {
 	Width, Height int
 }
